fe6d1b50c2414a08885802300dda3cbe7280c18b
ahmad-alhalwany <ahmad.s.alhalwnay@gmail.com>
2024-10-18 15:14:11 -0700

    [#12]: update settings.py

--- mystudybud/settings.py
+++ mystudybud/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # Default primary key field type
