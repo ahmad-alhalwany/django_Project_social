65a33ace86e04807b6ef53619f992f2e61d83d48
ahmad-alhalwany <ahmad.s.alhalwnay@gmail.com>
2024-10-18 16:05:47 -0700

    [#15]: update vercel.json and settings.py

--- mystudybud/settings.py
+++ mystudybud/settings.py
@@ -131,10 +131,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
