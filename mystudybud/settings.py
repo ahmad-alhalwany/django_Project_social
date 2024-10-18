2c28425ea09c1af1419c4a3c96bda296d1be2203
ahmad-alhalwany <ahmad.s.alhalwnay@gmail.com>
2024-10-18 15:18:20 -0700

    [#13]: update MEDIA_ROOT

--- mystudybud/settings.py
+++ mystudybud/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
