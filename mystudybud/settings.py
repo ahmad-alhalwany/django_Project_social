e71c310d5018fe5e1d7ec991f5275d9ce2c2d2ee
ahmad-alhalwany <ahmad.s.alhalwnay@gmail.com>
2024-10-18 14:31:39 -0700

    [#10]: update files for deploy

--- mystudybud/settings.py
+++ mystudybud/settings.py
@@ -129,18 +129,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
-# MEDIA_ROOT = BASE_DIR / 'static/images'
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
